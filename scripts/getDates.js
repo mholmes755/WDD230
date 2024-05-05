efd6e89284d1aafd739f3daa372620342d79bff1
mholmes755 <hol23026@byui.edu>
2024-05-04 20:21:28 -0600

    Toggle Light and Dark modes

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,5 @@
+// Get current date and time
+
 const lastModif = document.lastModified;
 const lastModifText = 'Last Modified: ' + lastModif;
 document.getElementById('lastModified').textContent = lastModifText;
@@ -8,10 +10,31 @@ const currentYear = new Date().toLocaleDateString('en-US', options);
 const copyrightYear = '&copy' + currentYear;
 document.getElementById('currentDate').innerHTML = copyrightYear;
 
+
+// Responsive Hamburger Menu
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+});
+
+
+// Toggle Dark/Light Mode
+
+const modeBtn = document.querySelector('#mode');
+const main = document.querySelector('main');
+
+modeBtn.addEventListener('click', () =>{
+    if (modeBtn.textContent.includes('🕶️')){
+        main.style.background = "#000"
+        main.style.color = "#fff";
+        modeBtn.textContent = "🔆";
+    }
+    else{
+        main.style.background = "#fff"
+        main.style.color = "#000";
+        modeBtn.textContent = "🕶️";
+    }
 });
